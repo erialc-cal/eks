3ef867914acd09976223062170065d6b56f59fed
Claire <61749901+erialc-cal@users.noreply.github.com>
2023-06-26 11:57:06 -0400

    additional
    - checks for fish data
    - finalise block tridiagonal solver (finally works)

---
+++ eks/block_tridiag.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+
+def Block_thomas_algo(y, m0, S0, A, B, E, ensemble_vars):
+    D = ensemble_vars[:,0]
+    M = np.zeros((r,r))
+    M = np.linalg.inv(S0) + A.T @ invE @ A 
+    invM = np.linalg.inv(M)
+    Tr = np.zeros((T,r,r)) 
+    q = np.zeros((T,r))
+    s = np.zeros((T,r))
+    Tr[0,:] = invM@ A.T @ invE
+    G = gradient_tridiag(y, m0, S0, A, B, E, ensemble_vars)
+    q[0,:] = - invM @ G[:,0]
+    for t in range(1,T):
+        D = np.diag(ensemble_vars[:,t])
+        invD = np.linalg.inv(D)
+        M = invE + A.T @ invE @ A + B.T @ invD @ B - A.T@invE@invM@(A.T @ invE).T
+        invM = np.linalg.inv(M)   
+        Tr[t,:] = invM @ A.T @ invE
+        q[t,:] = - invM @( G[:,t] - (A.T@invE).T @q[t-1,:] )
+    s[T-1,:] = q[T-1,:]
+    for t in range(T-2, -1, -1):
+        s[t,:] = q[t,:] + Tr[t,:]@s[t+1,:]
+        
+    return s
+
+
+def gradient_tridiag(y, m0, S0, A, B, E, ensemble_vars):
+    r = m0.shape[0]
+    T = y.shape[0]
+    G = np.zeros((r,T))
+    q = np.zeros((T,r))
+    invE = np.linalg.inv(E)
+    D = np.diag(ensemble_vars[:,0])
+    invD = np.linalg.inv(D)
+
+    for i in range(T-1):
+        D = np.diag(ensemble_vars[:,i])
+        invD = np.linalg.inv(D)
+        G[:,i] = - B.T @ invD@(y[i,:] - B@q[i,:]) - A.T @ invE @(q[i+1,:]-A@q[i,:]) + invE@(q[i,:]-A@q[i-1,:])
+    G[:,T-1] = - B.T @ invD@(y[T-1,:] - B@q[T-1,:])+ invE@(q[T-1,:]-A@q[T-2,:])
+    return G
+
+
+def newton_tridiag(y, m0, S0, A, B, E, ensemble_vars, max_iter = 200, eps= 10e-6):
+    # initialisation
+    
+    r = m0.shape[0]
+    T = y.shape[0]
+    n = y.shape[1]
+    
+    q = np.zeros((T,r))
+    
+    for iter in range(max_iter):
+        S = Block_thomas_algo(y, m0, S0, A, B, E, ensemble_vars)
+        qnew = q+S
+        if np.linalg.norm(S,2)<eps:
+            return qnew
+        q = qnew
+    
+    return q
+    
+    
+    
+    
+    
--- scripts/readme.txt
+++ scripts/readme.txt
@@ -4,8 +4,17 @@ Open terminal to use examples:
 
 Pupil example 
     To run a standard eks:
-    %run -i "scripts/pupil_example.py" --csv-dir 'data/ibl-pupil' --save-dir 'data/misc/pupil-test/' --diameter-s 0.99 --com-s 0.99 --eks_version "standard"
+    %run -i (or  %python) "scripts/pupil_example.py" --csv-dir 'data/ibl-pupil' --save-dir 'data/misc/pupil-test/' --diameter-s 0.99 --com-s 0.99 --eks_version "standard"
     To run an optimisation based eks:
     %run -i "scripts/pupil_example.py" --csv-dir 'data/ibl-pupil' --save-dir 'data/misc/pupil-test/' --diameter-s 0.99 --com-s 0.99 --eks_version "opti"
     
  
+
+Mirror-mouse example
+    %python scripts/multicam_example.py --csv-dir ./data/mirror-mouse --save-dir ./data/mirror-mouse/output --bodypart-list paw1LH paw2LF paw3RF paw4RH --camera-names top bot --eks_version opti
+    %python scripts/multicam_example.py --csv-dir ./data/mirror-mouse --save-dir ./data/mirror-mouse/output --bodypart-list paw1LH paw2LF paw3RF paw4RH --camera-names top bot --eks_version other
+
+
+Multiview paw example
+    %python scripts/multiview_paw_example.py --csv-dir ./data/ibl-paw --save-dir ./outputs --eks_version opti
+    %python scripts/multiview_paw_example.py --csv-dir ./data/ibl-paw --save-dir ./outputs --eks_version standard
