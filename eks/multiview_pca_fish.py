b305584dc5eded7d849508f8a682bd3439a2d16e
Claire <61749901+erialc-cal@users.noreply.github.com>
2023-07-20 09:34:29 -0400

    changes

--- eks/multiview_pca_fish.py
+++ eks/multiview_pca_fish.py
@@ -258,7 +258,6 @@ def filtering_pass_with_constraint(y, m0, S0, C, R, A, Q, ensemble_vars, D,L, ke
         constrained_keypoints_graph = pairwise(keypoint_ensemble_list)
         # all nodes are connected from bodyparts of interest
     # y.shape = (keypoints, time steps, views) 
-
     T = y.shape[1]  # number of time stpes
     n = len(keypoint_ensemble_list) # number of keypoints
     v = y.shape[2] # number of views
@@ -269,7 +268,7 @@ def filtering_pass_with_constraint(y, m0, S0, C, R, A, Q, ensemble_vars, D,L, ke
     for k, part in enumerate(keypoint_ensemble_list):
         # initial conditions
         for i in range(v):
-           R[i,i] = ensemble_vars[k][0][i]
+            R[i,i] = ensemble_vars[k][0][i]
         mf[k,0] =m0 + kalman_dot(y[k,0, :] - np.dot(C, m0), S0[k], C, R)
         Vf[k,0, :] = S0[k] - kalman_dot(np.dot(C, S0[k]), S0[k], C, R)
         S[k,0] = S0[k]
@@ -352,131 +351,132 @@ def hessian_distance(q, part,D, keypoint_ensemble_list, constrained_keypoints_gr
 
 
 
-# #%%%%% TEST 
-# folder = "/eks_opti"
-# operator = "/20210204_Quin/"
-# name = "img197707"
+#%%%%% TEST 
+folder = "/eks_opti"
+operator = "/20210204_Quin/"
+name = "img197707"
 
-# baseline = pd.read_csv("/Users/clairehe/Documents/GitHub/eks/data/misc/mirror-fish_ensemble-predictions/eks"+operator+name+".csv", header=[ 1, 2],index_col=0)
-# #new = pd.read_csv("/Users/clairehe/Documents/GitHub/eks/data/misc/one-video-mirror-fish-predictions"+folder+operator+name, header=[ 1, 2], index_col=0)
-# baseline0 = pd.read_csv("/Users/clairehe/Documents/GitHub/eks/data/misc/mirror-fish_ensemble-predictions/eks"+operator+name+".csv", header=[0, 1, 2],index_col=0)
-
-
-# # NOTE! replace this path with an absolute path where you want to save EKS outputs
-# eks_save_dir = '/Users/clairehe/Documents/GitHub/eks/data/misc/one-video-mirror-fish-predictions/eks_opti/'
-
-# # path for prediction csvs
-# file_path = '/Users/clairehe/Documents/GitHub/eks/data/misc/mirror-fish_ensemble-predictions'
-
-# # NOTE! replace these paths with the absolute paths to prediction csvs on your local computer
-# model_dirs = [
-#     file_path+"/network_0",
-#     file_path+"/network_1",
-#     file_path+"/network_2",
-#     file_path+"/network_3",
-#     file_path+"/network_4",
-# ]
+baseline = pd.read_csv("/Users/clairehe/Documents/GitHub/eks/data/misc/mirror-fish_ensemble-predictions/eks"+operator+name+".csv", header=[ 1, 2],index_col=0)
+#new = pd.read_csv("/Users/clairehe/Documents/GitHub/eks/data/misc/one-video-mirror-fish-predictions"+folder+operator+name, header=[ 1, 2], index_col=0)
+baseline0 = pd.read_csv("/Users/clairehe/Documents/GitHub/eks/data/misc/mirror-fish_ensemble-predictions/eks"+operator+name+".csv", header=[0, 1, 2],index_col=0)
 
 
-# #    'head', 'chin_base', 'chin1_4', 'chin_half','chin3_4', 'chin_tip', 'mid', 'fork',
-# #   'stripeA', 'stripeP', 'tail_neck', 'dorsal', 'anal', 'caudal_d', 'caudal_v',
+# NOTE! replace this path with an absolute path where you want to save EKS outputs
+eks_save_dir = '/Users/clairehe/Documents/GitHub/eks/data/misc/one-video-mirror-fish-predictions/eks_opti/'
 
-# image_path = "/Users/clairehe/Documents/GitHub/eks/data/mirror-fish/labeled-data"
-# im = plt.imread(image_path+operator+name+".png")
-# plt.imshow(im)
-# plt.suptitle("labeled "+name)
+# path for prediction csvs
+file_path = '/Users/clairehe/Documents/GitHub/eks/data/misc/mirror-fish_ensemble-predictions'
 
-
-# session = '20210204_Quin'
-# frame = 'img197707.csv'
-# smooth_param = 0.01
-# quantile_keep_pca = 50
-# # Get markers list from networks
-# markers_list = []
-# for model_dir in model_dirs:
-#     csv_file = os.path.join(model_dir, session, frame)
-#     df_tmp = pd.read_csv(csv_file, header=[0, 1, 2], index_col=0)
-#     keypoint_names = [l[1] for l in df_tmp.columns[::3]]
-#     markers_tmp = convert_lp_dlc(df_tmp, keypoint_names, model_name=tracker_name)
-#     markers_list.append(markers_tmp)
-
-# # Ensemble
-# scaled_dict = []
-# good_frames_dict = []
-# good_preds_dict = []
-# ensemble_vars_dict = []
-# means_camera_dict = []
-# for n, keypoint_ensemble in enumerate(keypoint_ensemble_list):
-#     markers_list_cameras = [[] for i in range(num_cameras)]
-#     for m in markers_list:
-#         for camera in range(num_cameras):
-#             markers_list_cameras[camera].append(
-#                 m[[key for key in m.keys() 
-#                    if camera_names[camera] in key 
-#                    and 'likelihood' not in key 
-#                    and keypoint_ensemble in key]
-#                  ]
-#             )
-#     # ENSEMBLING PER KEYPOINTS
-#     scaled_ensemble_preds, good_frames, good_scaled_ensemble_preds,ensemble_vars,means_camera = ensembling_multiview(markers_list_cameras, keypoint_ensemble, smooth_param, quantile_keep_pca, camera_names, plot=True)
-#     scaled_dict.append(scaled_ensemble_preds)
-#     good_frames_dict.append(good_frames)
-#     good_preds_dict.append(good_scaled_ensemble_preds)
-#     ensemble_vars_dict.append(ensemble_vars)
-#     means_camera_dict.append(means_camera)
-# ensemble_vars = np.array(ensemble_vars_dict)
+# NOTE! replace these paths with the absolute paths to prediction csvs on your local computer
+model_dirs = [
+    file_path+"/network_0",
+    file_path+"/network_1",
+    file_path+"/network_2",
+    file_path+"/network_3",
+    file_path+"/network_4",
+]
 
 
-# stacked_preds,ensemble_pca,ensemble_ex_var,ensemble_pcs,good_ensemble_pcs =  multiview_pca_bodyparts(scaled_dict,good_preds_dict,good_frames_dict)
+#    'head', 'chin_base', 'chin1_4', 'chin_half','chin3_4', 'chin_tip', 'mid', 'fork',
+#   'stripeA', 'stripeP', 'tail_neck', 'dorsal', 'anal', 'caudal_d', 'caudal_v',
 
-# y_obs = np.asarray(stacked_preds)
-
-# #compute center of mass
-# #latent variables (observed)
-# good_z_t_obs = good_ensemble_pcs #latent variables - true 3D pca
-
-# n, T, v = y_obs.shape
-
-# ##### Set values for kalman filter #####
-# m0 = np.asarray([0.0, 0.0, 0.0]) # initial state: mean
-# S0 = np.zeros((nkeys,m0.shape[0], m0.shape[0] ))
-# d_t = {key: None for key in range(nkeys)}
-# # need different variance for each bodyparts 
-# for k in range(n):
-#     S0[k,:,:] =  np.asarray([[np.var(good_z_t_obs[k][:,0]), 0.0, 0.0], [0.0, np.var(good_z_t_obs[k][:,1]), 0.0], [0.0, 0.0, np.var(good_z_t_obs[k][:,2])]]) # diagonal: var
-#     d_t[k] = good_z_t_obs[k][1:] - good_z_t_obs[k][:-1]
-
-#     Q = smooth_param*np.cov(d_t[k].T)
-
-# A = np.asarray([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]) #state-transition matrix,
-# # Q = np.asarray([[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]]) #state covariance matrix?????
+image_path = "/Users/clairehe/Documents/GitHub/eks/data/mirror-fish/labeled-data"
+im = plt.imread(image_path+operator+name+".png")
+plt.imshow(im)
+plt.suptitle("labeled "+name)
 
 
-# C = ensemble_pca.components_.T # Measurement function is inverse transform of PCA
-# R = np.eye(ensemble_pca.components_.shape[1]) # placeholder diagonal matrix for ensemble variance
+session = '20210204_Quin'
+frame = 'img197707.csv'
+smooth_param = 0.01
+quantile_keep_pca = 50
+# Get markers list from networks
+markers_list = []
+for model_dir in model_dirs:
+    csv_file = os.path.join(model_dir, session, frame)
+    df_tmp = pd.read_csv(csv_file, header=[0, 1, 2], index_col=0)
+    keypoint_names = [l[1] for l in df_tmp.columns[::3]]
+    markers_tmp = convert_lp_dlc(df_tmp, keypoint_names, model_name=tracker_name)
+    markers_list.append(markers_tmp)
 
-# print(f"filtering ...")
-# mfc, Vfc, Sc = filtering_pass_with_constraint(y_obs, m0, S0, C, R, A, Q,ensemble_vars, D, L,keypoint_ensemble_list, constrained_keypoints_graph=[('fork','mid'),('chin_base','fork')], mu=0)
+# Ensemble
+scaled_dict = []
+good_frames_dict = []
+good_preds_dict = []
+ensemble_vars_dict = []
+means_camera_dict = []
+for n, keypoint_ensemble in enumerate(keypoint_ensemble_list):
+    markers_list_cameras = [[] for i in range(num_cameras)]
+    for m in markers_list:
+        for camera in range(num_cameras):
+            markers_list_cameras[camera].append(
+                m[[key for key in m.keys() 
+                    if camera_names[camera] in key 
+                    and 'likelihood' not in key 
+                    and keypoint_ensemble in key]
+                  ]
+            )
+    # ENSEMBLING PER KEYPOINTS
+    scaled_ensemble_preds, good_frames, good_scaled_ensemble_preds,ensemble_vars,means_camera = ensembling_multiview(markers_list_cameras, keypoint_ensemble, smooth_param, quantile_keep_pca, camera_names, plot=True)
+    scaled_dict.append(scaled_ensemble_preds)
+    good_frames_dict.append(good_frames)
+    good_preds_dict.append(good_scaled_ensemble_preds)
+    ensemble_vars_dict.append(ensemble_vars)
+    means_camera_dict.append(means_camera)
+ensemble_vars = np.array(ensemble_vars_dict)
 
 
-# # Do the smoothing step
-# print("done filtering")
-# y_m_filt = {key: None for key in range(n)}
-# y_v_filt = {key: None for key in range(n)}
-# y_m_smooth = {key: None for key in range(n)}
-# y_v_smooth = {key: None for key in range(n)}
-# ms = {key: None for key in range(n)}
-# Vs = {key: None for key in range(n)}
-# for k in range(n):
-#     y_m_filt[k] = np.dot(C, mfc[k].T).T
-#     y_v_filt[k] = np.swapaxes(np.dot(C, np.dot(Vfc[k], C.T)), 0, 1)
-#     print(f"smoothing {keypoint_ensemble_list[k]}...")
-#     ms[k], Vs[k], _ = smooth_backward(y_obs[k], mfc[k], Vfc[k], Sc[k], A, Q, C)
-#     print("done smoothing")
+stacked_preds,ensemble_pca,ensemble_ex_var,ensemble_pcs,good_ensemble_pcs =  multiview_pca_bodyparts(scaled_dict,good_preds_dict,good_frames_dict)
 
-#     # Smoothed posterior over yb
-#     y_m_smooth[k] = np.dot(C, ms[k].T).T
-#     y_v_smooth[k] = np.swapaxes(np.dot(C, np.dot(Vs[k], C.T)), 0, 1)
+y_obs = np.asarray(stacked_preds)
+
+#compute center of mass
+#latent variables (observed)
+good_z_t_obs = good_ensemble_pcs #latent variables - true 3D pca
+
+n, T, v = y_obs.shape
+
+##### Set values for kalman filter #####
+m0 = np.asarray([0.0, 0.0, 0.0]) # initial state: mean
+S0 = np.zeros((nkeys,m0.shape[0], m0.shape[0] ))
+d_t = {key: None for key in range(nkeys)}
+# need different variance for each bodyparts 
+for k in range(n):
+    S0[k,:,:] =  np.asarray([[np.var(good_z_t_obs[k][:,0]), 0.0, 0.0], [0.0, np.var(good_z_t_obs[k][:,1]), 0.0], [0.0, 0.0, np.var(good_z_t_obs[k][:,2])]]) # diagonal: var
+    d_t[k] = good_z_t_obs[k][1:] - good_z_t_obs[k][:-1]
+
+    Q = smooth_param*np.cov(d_t[k].T)
+
+A = np.asarray([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]) #state-transition matrix,
+# Q = np.asarray([[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]]) #state covariance matrix?????
+
+
+C = ensemble_pca.components_.T # Measurement function is inverse transform of PCA
+R = np.eye(ensemble_pca.components_.shape[1]) # placeholder diagonal matrix for ensemble variance
+
+print(f"filtering ...")
+mfc, Vfc, Sc = filtering_pass_with_constraint(y_obs, m0, S0, C, R, A, Q,ensemble_vars, D, L,keypoint_ensemble_list, constrained_keypoints_graph=[('fork','mid'),('chin_base','fork')], mu=0)
+
+
+# Do the smoothing step
+print("done filtering")
+y_m_filt = {key: None for key in range(n)}
+y_v_filt = {key: None for key in range(n)}
+y_m_smooth = {key: None for key in range(n)}
+y_v_smooth = {key: None for key in range(n)}
+ms = {key: None for key in range(n)}
+Vs = {key: None for key in range(n)}
+for k in range(n):
+    y_m_filt[k] = np.dot(C, mfc[k].T).T
+    y_v_filt[k] = np.swapaxes(np.dot(C, np.dot(Vfc[k], C.T)), 0, 1)
+    print(f"smoothing {keypoint_ensemble_list[k]}...")
+    ms[k], Vs[k], _ = smooth_backward(y_obs[k], mfc[k], Vfc[k], Sc[k], A, Q, C)
+
+    print("done smoothing")
+
+    # Smoothed posterior over yb
+    y_m_smooth[k] = np.dot(C, ms[k].T).T
+    y_v_smooth[k] = np.swapaxes(np.dot(C, np.dot(Vs[k], C.T)), 0, 1)
 
 
 
